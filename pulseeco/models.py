d922733c62779dd90d55f582426aedccae75fadf
Martin Popovski <martinkozle@yahoo.com>
2023-10-28 16:08:55 +0200

    Add descriptions to model arguments

--- pulseeco/models.py
+++ pulseeco/models.py
@@ -9,21 +9,31 @@ from .enums import DataValueType, SensorStatus, SensorType  # noqa: TCH001
 
 
 class Sensor(BaseModel):
-    sensor_id: str = Field(alias="sensorId")
-    position: str
-    comments: str
-    type: SensorType
-    description: str
-    status: SensorStatus
+    sensor_id: str = Field(alias="sensorId", description="The unique ID of the sensor")
+    position: str = Field(
+        description="Latitude and longitude GPS coordinates of the sensor"
+    )
+    comments: str = Field(description="Any other comments about the sensor")
+    type: SensorType = Field(description="The type of the sensor")
+    description: str = Field(description="Short description / nome")
+    status: SensorStatus = Field(description="The current status of the sensor")
 
 
 class DataValue(BaseModel):
-    sensor_id: str = Field(alias="sensorId")
-    stamp: datetime.datetime
-    type: DataValueType
-    position: Optional[str]  # noqa: UP007
-    value: int
-    year: Optional[int] = None  # noqa: UP007
+    sensor_id: str = Field(alias="sensorId", description="The unique ID of the sensor")
+    stamp: datetime.datetime = Field(
+        description="Timestamp of when the measurement was taken"
+    )
+    type: DataValueType = Field(description="The type of the data value taken")
+    position: Optional[str] = Field(  # noqa: UP007
+        description="Latitude and longitude GPS coordinates of the sensor"
+    )
+    value: int = Field(description="The actual value of the measurement taken")
+    year: Optional[int] = Field(  # noqa: UP007
+        default=None,
+        description="Year when the measurement was taken"
+        ", not included with newer data, prefer stamp",
+    )
 
 
 class OverallValues(BaseModel):
@@ -38,5 +48,5 @@ class OverallValues(BaseModel):
 
 
 class Overall(BaseModel):
-    city_name: str = Field(alias="cityName")
-    values: OverallValues
+    city_name: str = Field(alias="cityName", description="The city name")
+    values: OverallValues = Field(description="The overall values for the city")
